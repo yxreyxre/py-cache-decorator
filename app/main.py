8b61cc7920adf23b21b9f38e6103e67dfa91cc3c
Alex <Rieznik.O.A@nmu.one>
2025-07-10 18:00:42 +0300

    Solution#1

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from typing import Callable
-
+saved_cache = []
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def inner(*args, **kwargs) -> str:
+        if func(*args, **kwargs) not in saved_cache:
+            print("Calculating new result")
+            saved_cache.append(func(*args, **kwargs))
+            return func(*args, **kwargs)
+        else:
+            print("Getting from cache")
+            return saved_cache[func(*args, **kwargs)]
+    return inner
