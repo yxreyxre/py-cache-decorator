08113617b9ce47d947a9d3085d805f1115cb1d9c
Alex <Rieznik.O.A@nmu.one>
2025-07-10 19:44:58 +0300

    Solution#3

--- app/main.py
+++ app/main.py
@@ -1,23 +1,17 @@
 from typing import Callable
+from typing import Any
 saved_cache = {}
 
 def cache(func: Callable) -> Callable:
-    def inner(*args, **kwargs) -> None:
-        if func(args, kwargs) not in saved_cache:
+    def inner(*args) -> Any:
+        key = args
+        if key not in saved_cache:
             print("Calculating new result")
-            saved_cache[args, kwargs] = func(args, kwargs)
-            return func(args, kwargs)
+            result = func(*args)
+            saved_cache[key] = result
+            return result
         else:
             print("Getting from cache")
-            return saved_cache[args, kwargs]
+            return saved_cache[key]
     return inner
 
-@cache
-def long_time_func(a: int, b: int, c: int) -> int:
-    return (a ** b ** c) % (a * c)
-
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func(1, 2, 3)
